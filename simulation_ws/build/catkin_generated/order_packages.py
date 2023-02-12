cdb96a642149ba9ff530c7e98199698cd04bf4f7
sc20tkkc <sc20tkkc@leeds.ac.uk>
2023-02-12 15:09:00 +0000

    Initial catkin_ws setup.

---
+++ simulation_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/takofish/FYP-2022-23/simulation_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/takofish/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/takofish/catkin_ws/devel;/opt/ros/noetic' != '' else []
