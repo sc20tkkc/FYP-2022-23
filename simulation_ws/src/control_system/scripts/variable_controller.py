16364d4f91e8af4925cd0f4c626e3895be665153
sc20tkkc <sc20tkkc@leeds.ac.uk>
2023-03-28 18:23:45 +0100

    Added variable mappings to variable_controller

--- simulation_ws/src/control_system/scripts/variable_controller.py
+++ simulation_ws/src/control_system/scripts/variable_controller.py
@@ -14,10 +14,34 @@ import time
 import sys
 import numpy
 import json
-args = sys.argv[1:]
 
-def init_consts():
-    global args
+# Test file used to experiment with variable passing
+
+"""
+Variable Mappings:
+speed_recover = 0
+time_recover = 1
+speed_search_ = 2
+speed_search_forward = 3
+speed_search_angular = 4
+strategy_search = 5
+time_spin_min = 6
+time_spin_max = 7
+threshold_proximity_lost = 8
+threshold_proximity_found = 9
+speed_attack = 10
+threshold_proximity_veer = 11
+speed_veer_linear = 12
+speed_veer_angular = 13
+time_stalemate = 14
+threshold_proximity_ram = 15
+speed_ram = 16
+threshold_proximity_swerve = 17
+speed_swerve_linear = 18
+speed_swerve_angular = 19
+"""
+
+args = sys.argv[1:]
 
 for arg in args:
     solution = json.loads(arg)
@@ -25,4 +49,3 @@ for arg in args:
         for i in range(0,len(solution)):
             print(i, solution[i])
 
-init_consts()
